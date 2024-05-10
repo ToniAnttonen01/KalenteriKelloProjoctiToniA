b1f032b408fa3b85719ec895ba56c3bdb89fd4c7
ViktorGulis <142294827+ViktorGulis@users.noreply.github.com>
2024-05-10 12:58:13 +0300

    Desing alotettu jatkuu

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,20 +28,116 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.panelMenu = new System.Windows.Forms.Panel();
+            this.panelLogo = new System.Windows.Forms.Panel();
+            this.btnClock = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.panelMenu.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // panelMenu
+            // 
+            this.panelMenu.BackColor = System.Drawing.Color.DarkRed;
+            this.panelMenu.Controls.Add(this.button3);
+            this.panelMenu.Controls.Add(this.button2);
+            this.panelMenu.Controls.Add(this.button1);
+            this.panelMenu.Controls.Add(this.btnClock);
+            this.panelMenu.Controls.Add(this.panelLogo);
+            this.panelMenu.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panelMenu.Location = new System.Drawing.Point(0, 0);
+            this.panelMenu.Name = "panelMenu";
+            this.panelMenu.Size = new System.Drawing.Size(201, 498);
+            this.panelMenu.TabIndex = 0;
+            // 
+            // panelLogo
+            // 
+            this.panelLogo.BackColor = System.Drawing.Color.Maroon;
+            this.panelLogo.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelLogo.Location = new System.Drawing.Point(0, 0);
+            this.panelLogo.Name = "panelLogo";
+            this.panelLogo.Size = new System.Drawing.Size(201, 70);
+            this.panelLogo.TabIndex = 0;
+            // 
+            // btnClock
+            // 
+            this.btnClock.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnClock.FlatAppearance.BorderSize = 0;
+            this.btnClock.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnClock.ForeColor = System.Drawing.Color.Gainsboro;
+            this.btnClock.Location = new System.Drawing.Point(0, 70);
+            this.btnClock.Name = "btnClock";
+            this.btnClock.Size = new System.Drawing.Size(201, 54);
+            this.btnClock.TabIndex = 1;
+            this.btnClock.Text = "Clock";
+            this.btnClock.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnClock.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.ForeColor = System.Drawing.Color.Gainsboro;
+            this.button1.Location = new System.Drawing.Point(0, 124);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(201, 54);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Video kohta 2:24";
+            this.button1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.ForeColor = System.Drawing.Color.Gainsboro;
+            this.button2.Location = new System.Drawing.Point(0, 178);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(201, 54);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Clock";
+            this.button2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button3.FlatAppearance.BorderSize = 0;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.ForeColor = System.Drawing.Color.Gainsboro;
+            this.button3.Location = new System.Drawing.Point(0, 232);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(201, 54);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Clock";
+            this.button3.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button3.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(532, 691);
+            this.BackColor = System.Drawing.Color.Indigo;
+            this.ClientSize = new System.Drawing.Size(892, 498);
+            this.Controls.Add(this.panelMenu);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.panelMenu.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panelMenu;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnClock;
+        private System.Windows.Forms.Panel panelLogo;
     }
 }
 
