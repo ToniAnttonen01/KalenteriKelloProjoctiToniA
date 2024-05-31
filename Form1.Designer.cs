d76c7b123cf1f82869966379b39f755ea3fbaa28
ToniAnttonen01 <toni.anttonen@hotmail.com>
2024-05-31 10:34:23 +0300

    Desing_jatkuu random_color_valmis?

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,76 +29,80 @@
         private void InitializeComponent()
         {
             this.panelMenu = new System.Windows.Forms.Panel();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button1 = new System.Windows.Forms.Button();
+            this.btnNotepad = new System.Windows.Forms.Button();
+            this.btnCalculator = new System.Windows.Forms.Button();
+            this.btnCalendar = new System.Windows.Forms.Button();
             this.btnClock = new System.Windows.Forms.Button();
             this.panelLogo = new System.Windows.Forms.Panel();
+            this.panelTitleBar = new System.Windows.Forms.Panel();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
             this.panelMenu.SuspendLayout();
+            this.panelLogo.SuspendLayout();
+            this.panelTitleBar.SuspendLayout();
             this.SuspendLayout();
             // 
             // panelMenu
             // 
             this.panelMenu.BackColor = System.Drawing.Color.DarkRed;
-            this.panelMenu.Controls.Add(this.button3);
-            this.panelMenu.Controls.Add(this.button2);
-            this.panelMenu.Controls.Add(this.button1);
+            this.panelMenu.Controls.Add(this.btnNotepad);
+            this.panelMenu.Controls.Add(this.btnCalculator);
+            this.panelMenu.Controls.Add(this.btnCalendar);
             this.panelMenu.Controls.Add(this.btnClock);
             this.panelMenu.Controls.Add(this.panelLogo);
             this.panelMenu.Dock = System.Windows.Forms.DockStyle.Left;
             this.panelMenu.Location = new System.Drawing.Point(0, 0);
-            this.panelMenu.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.panelMenu.Name = "panelMenu";
-            this.panelMenu.Size = new System.Drawing.Size(268, 613);
+            this.panelMenu.Size = new System.Drawing.Size(201, 498);
             this.panelMenu.TabIndex = 0;
             // 
-            // button3
+            // btnNotepad
             // 
-            this.button3.Dock = System.Windows.Forms.DockStyle.Top;
-            this.button3.FlatAppearance.BorderSize = 0;
-            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button3.ForeColor = System.Drawing.Color.Gainsboro;
-            this.button3.Location = new System.Drawing.Point(0, 284);
-            this.button3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.button3.Name = "button3";
-            this.button3.Padding = new System.Windows.Forms.Padding(12, 0, 0, 0);
-            this.button3.Size = new System.Drawing.Size(268, 66);
-            this.button3.TabIndex = 4;
-            this.button3.Text = "Clock";
-            this.button3.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.button3.UseVisualStyleBackColor = true;
+            this.btnNotepad.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnNotepad.FlatAppearance.BorderSize = 0;
+            this.btnNotepad.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnNotepad.ForeColor = System.Drawing.Color.Gainsboro;
+            this.btnNotepad.Location = new System.Drawing.Point(0, 232);
+            this.btnNotepad.Name = "btnNotepad";
+            this.btnNotepad.Padding = new System.Windows.Forms.Padding(9, 0, 0, 0);
+            this.btnNotepad.Size = new System.Drawing.Size(201, 54);
+            this.btnNotepad.TabIndex = 4;
+            this.btnNotepad.Text = "Notepad";
+            this.btnNotepad.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnNotepad.UseVisualStyleBackColor = true;
+            this.btnNotepad.Click += new System.EventHandler(this.btnNotepad_Click);
             // 
-            // button2
+            // btnCalculator
             // 
-            this.button2.Dock = System.Windows.Forms.DockStyle.Top;
-            this.button2.FlatAppearance.BorderSize = 0;
-            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button2.ForeColor = System.Drawing.Color.Gainsboro;
-            this.button2.Location = new System.Drawing.Point(0, 218);
-            this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.button2.Name = "button2";
-            this.button2.Padding = new System.Windows.Forms.Padding(12, 0, 0, 0);
-            this.button2.Size = new System.Drawing.Size(268, 66);
-            this.button2.TabIndex = 3;
-            this.button2.Text = "Clock";
-            this.button2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.button2.UseVisualStyleBackColor = true;
+            this.btnCalculator.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnCalculator.FlatAppearance.BorderSize = 0;
+            this.btnCalculator.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCalculator.ForeColor = System.Drawing.Color.Gainsboro;
+            this.btnCalculator.Location = new System.Drawing.Point(0, 178);
+            this.btnCalculator.Name = "btnCalculator";
+            this.btnCalculator.Padding = new System.Windows.Forms.Padding(9, 0, 0, 0);
+            this.btnCalculator.Size = new System.Drawing.Size(201, 54);
+            this.btnCalculator.TabIndex = 3;
+            this.btnCalculator.Text = "Calculator";
+            this.btnCalculator.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnCalculator.UseVisualStyleBackColor = true;
+            this.btnCalculator.Click += new System.EventHandler(this.btnCalculator_Click);
             // 
-            // button1
+            // btnCalendar
             // 
-            this.button1.Dock = System.Windows.Forms.DockStyle.Top;
-            this.button1.FlatAppearance.BorderSize = 0;
-            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button1.ForeColor = System.Drawing.Color.Gainsboro;
-            this.button1.Location = new System.Drawing.Point(0, 152);
-            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.button1.Name = "button1";
-            this.button1.Padding = new System.Windows.Forms.Padding(12, 0, 0, 0);
-            this.button1.Size = new System.Drawing.Size(268, 66);
-            this.button1.TabIndex = 2;
-            this.button1.Text = "Video kohta 3:32";
-            this.button1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.button1.UseVisualStyleBackColor = true;
+            this.btnCalendar.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnCalendar.FlatAppearance.BorderSize = 0;
+            this.btnCalendar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCalendar.ForeColor = System.Drawing.Color.Gainsboro;
+            this.btnCalendar.Location = new System.Drawing.Point(0, 124);
+            this.btnCalendar.Name = "btnCalendar";
+            this.btnCalendar.Padding = new System.Windows.Forms.Padding(9, 0, 0, 0);
+            this.btnCalendar.Size = new System.Drawing.Size(201, 54);
+            this.btnCalendar.TabIndex = 2;
+            this.btnCalendar.Text = "Video kohta 12:44 Calendar";
+            this.btnCalendar.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnCalendar.UseVisualStyleBackColor = true;
+            this.btnCalendar.Click += new System.EventHandler(this.btnCalendar_Click);
             // 
             // btnClock
             // 
@@ -106,38 +110,76 @@
             this.btnClock.FlatAppearance.BorderSize = 0;
             this.btnClock.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnClock.ForeColor = System.Drawing.Color.Gainsboro;
-            this.btnClock.Location = new System.Drawing.Point(0, 86);
-            this.btnClock.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnClock.Location = new System.Drawing.Point(0, 70);
             this.btnClock.Name = "btnClock";
-            this.btnClock.Padding = new System.Windows.Forms.Padding(12, 0, 0, 0);
-            this.btnClock.Size = new System.Drawing.Size(268, 66);
+            this.btnClock.Padding = new System.Windows.Forms.Padding(9, 0, 0, 0);
+            this.btnClock.Size = new System.Drawing.Size(201, 54);
             this.btnClock.TabIndex = 1;
             this.btnClock.Text = "Clock";
             this.btnClock.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.btnClock.UseVisualStyleBackColor = true;
+            this.btnClock.Click += new System.EventHandler(this.btnClock_Click);
             // 
             // panelLogo
             // 
             this.panelLogo.BackColor = System.Drawing.Color.Maroon;
+            this.panelLogo.Controls.Add(this.label1);
             this.panelLogo.Dock = System.Windows.Forms.DockStyle.Top;
             this.panelLogo.Location = new System.Drawing.Point(0, 0);
-            this.panelLogo.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.panelLogo.Name = "panelLogo";
-            this.panelLogo.Padding = new System.Windows.Forms.Padding(12, 0, 0, 0);
-            this.panelLogo.Size = new System.Drawing.Size(268, 86);
+            this.panelLogo.Padding = new System.Windows.Forms.Padding(9, 0, 0, 0);
+            this.panelLogo.Size = new System.Drawing.Size(201, 70);
             this.panelLogo.TabIndex = 0;
+            // 
+            // panelTitleBar
+            // 
+            this.panelTitleBar.BackColor = System.Drawing.Color.DarkRed;
+            this.panelTitleBar.Controls.Add(this.lblTitle);
+            this.panelTitleBar.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelTitleBar.Location = new System.Drawing.Point(201, 0);
+            this.panelTitleBar.Name = "panelTitleBar";
+            this.panelTitleBar.Size = new System.Drawing.Size(691, 70);
+            this.panelTitleBar.TabIndex = 1;
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitle.ForeColor = System.Drawing.SystemColors.Control;
+            this.lblTitle.Location = new System.Drawing.Point(302, 22);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(62, 24);
+            this.lblTitle.TabIndex = 0;
+            this.lblTitle.Text = "Home";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.Control;
+            this.label1.Location = new System.Drawing.Point(25, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(146, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Toni Anttonen";
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Indigo;
-            this.ClientSize = new System.Drawing.Size(1189, 613);
+            this.ClientSize = new System.Drawing.Size(892, 498);
+            this.Controls.Add(this.panelTitleBar);
             this.Controls.Add(this.panelMenu);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Name = "Form1";
             this.Text = "Form1";
             this.panelMenu.ResumeLayout(false);
+            this.panelLogo.ResumeLayout(false);
+            this.panelLogo.PerformLayout();
+            this.panelTitleBar.ResumeLayout(false);
+            this.panelTitleBar.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -145,11 +187,14 @@
         #endregion
 
         private System.Windows.Forms.Panel panelMenu;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnNotepad;
+        private System.Windows.Forms.Button btnCalculator;
+        private System.Windows.Forms.Button btnCalendar;
         private System.Windows.Forms.Button btnClock;
         private System.Windows.Forms.Panel panelLogo;
+        private System.Windows.Forms.Panel panelTitleBar;
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -12,9 +12,78 @@ namespace KalenteriKelloProjoctiToniA
 {
     public partial class Form1 : Form
     {
+
+        private Button currentButton;
+        private Random random;
+        private int tempIndex;
+
         public Form1()
         {
             InitializeComponent();
+            random = new Random();
+        }
+
+        private Color SelectThemeColor()
+        {
+            int index = random.Next(ThemeColor.ColorList.Count);
+            while(tempIndex == index)
+            {
+                index = random.Next(ThemeColor.ColorList.Count);
+            }
+            tempIndex = index;
+            string color = ThemeColor.ColorList[index];
+            return ColorTranslator.FromHtml(color);
+        }
+
+        private void ActivateButton(object btnSender)
+        {
+            if(btnSender != null)
+            {
+            if(currentButton != (Button)btnSender)
+                {
+                    DisableButton();
+                    Color color = SelectThemeColor();
+                    currentButton = (Button)btnSender;
+                    currentButton.BackColor = color;
+                    currentButton.ForeColor = Color.White;
+                    currentButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+                    panelTitleBar.BackColor = color;
+                    panelLogo.BackColor
+                }
+            }
+        }
+
+        private void DisableButton()
+        {
+            foreach(Control previousBtn in panelMenu.Controls)
+            {
+                if(previousBtn.GetType() == typeof(Button))
+                {
+                    previousBtn.BackColor = Color.FromArgb(139,0,0);
+                    previousBtn.ForeColor = Color.Gainsboro;
+                    previousBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+                }
+            }
+        }
+
+        private void btnClock_Click(object sender, EventArgs e)
+        {
+            ActivateButton(sender);
+        }
+
+        private void btnCalendar_Click(object sender, EventArgs e)
+        {
+            ActivateButton(sender);
+        }
+
+        private void btnCalculator_Click(object sender, EventArgs e)
+        {
+            ActivateButton(sender);
+        }
+
+        private void btnNotepad_Click(object sender, EventArgs e)
+        {
+            ActivateButton(sender);
         }
     }
 }
